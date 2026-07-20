baebe6ee63acd26f00ca396566aaf7ce45f34e78
pbkx <93405617+pbkx@users.noreply.github.com>
2026-07-20 21:27:25 +0000

    Finished lesson 1

--- lesson1/classwork/practice.js
+++ lesson1/classwork/practice.js
@@ -1,25 +1,35 @@
 // Problem 1
 // Create a variable for your favorite number and print it.
 
-
+let favorite_number = 11;
+console.log(favorite_number);
 
 // Problem 2
 // Create a variable for your height in meters and print it.
 
-
+let height = 1.72;
+console.log(height);
 
 // Problem 3
 // Create a variable for your favorite animal and print it in a full sentence.
 
-
+let favorite_animal = "tiger";
+console.log("My favorite animal is", favorite_animal);
 
 // Problem 4
 // Create two variables: one for your first name and one for your last name.
 // Print them on 2 separate lines.
 
-
+let first_name = "Max";
+let last_name = "Ch";
+console.log(first_name);
+console.log(last_name);
 
 // Problem 5
 // Create 3 variables: your first name, your age, and your favorite color.
 // Print them on the same line in a full sentence.
 
+let firstname = "Max";
+let age = 17;
+let favorite_color = "blue";
+console.log("My name is", firstname, ", I am", age, "years old, and my favorite color is", favorite_color, ".");
---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello world!");
+console.log("My name is Max");
+console.log('My favorite food is pizza!');
+
+// Error: quotation marks mismatched
+// console.log("I love dogs!');
