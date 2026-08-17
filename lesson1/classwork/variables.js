72fadf2f007394baee74c610b5372efabbf77d0d
pbkx <93405617+pbkx@users.noreply.github.com>
2026-08-17 18:25:20 +0000

    Finished lesson 1

---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,15 @@
+let x = 5;  // number: whole-number or decimal value
+console.log(x);
+
+let number = 3.97;
+console.log(number);
+
+// Changing the value stored inside the variable
+x = 9;  // You only use the let keyword when firts defining your variable
+console.log(x);
+
+let favorite_food = "pizza";  // string: text enclosed in quotes
+console.log(favorite_food);
+console.log("My favorite food is", favorite_food)
+
+console.log(4.21, "dogs", 18, x);
